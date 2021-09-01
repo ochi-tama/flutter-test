5ac29039b8b6606eb89437591840913d3dc70ee3
ochi-tama <per.raid@gmail.com>
2021-09-01 23:36:31 +0900

    feat: add complete function to wearingtimer controller

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,8 +2,10 @@ import 'package:riverpod/riverpod.dart';
 
 import 'application/wearing_timer/cancel/cancel_interactor.dart';
 import 'application/wearing_timer/cancel/interface/cancel_input_port.dart';
+import 'application/wearing_timer/complete/complete_interactor.dart';
 import 'application/wearing_timer/complete/complete_presenter.dart';
 import 'application/wearing_timer/complete/data/complete_presenter_data.dart';
+import 'application/wearing_timer/complete/interfaces/complete_input_port.dart';
 import 'application/wearing_timer/find/data/find_presenter_data.dart';
 import 'application/wearing_timer/find/find_input_port.dart';
 import 'application/wearing_timer/find/find_interactor.dart';
@@ -88,3 +90,10 @@ final timerViewModelProvider =
 final completePresenterProvider =
     StateNotifierProvider<CompletePresenter, CompletePresenterData>(
         (ref) => CompletePresenter());
+
+/// No Doc
+final completeInteractorProvider = Provider<CompleteInputPort>((ref) {
+  final repository = ref.read(wearingTimerRepositoryProvider);
+  final outputPort = ref.read(completePresenterProvider.notifier);
+  return CompleteInteractor(repository: repository, outputPort: outputPort);
+});
--- test/application/wearing_timer/wearing_timer_controller_test.dart
+++ test/application/wearing_timer/wearing_timer_controller_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:test_app/application/wearing_timer/complete/data/complete_presenter_data.dart';
+import 'package:test_app/application/wearing_timer/wearing_timer_controller.dart';
 import 'package:test_app/provider.dart';
 
 import '../../utils/data/fake_wearing_timer.dart';
@@ -50,6 +52,32 @@ void main() {
 
       expect(container.read(findPresenterNotifierProvider),
           equals(TestWearingTimerData.findPresenterDataTimerNotStarted()));
+    });
+
+    group("complete function", () {
+      test(
+          'Given a timer is already registerd '
+          'When complete function is triggerd '
+          'Then a completed timer data is reflected to the presenterData',
+          () async {
+        final testData = TestWearingTimerData.wearingTimerStarted();
+        final container = ProviderContainer(overrides: [
+          wearingTimerRepositoryProvider.overrideWithProvider(Provider(
+              (ref) => FakeWearingTimerRepositoryImpl(wearingTimer: testData)))
+        ]);
+
+        final completeInteractor = container.read(completeInteractorProvider);
+        final wearingTimerController =
+            WearingTimerController(completeInputPort: completeInteractor);
+
+        await wearingTimerController.completeWearingTimer();
+
+        final expected = container.read(completePresenterProvider);
+        expect(
+            expected,
+            equals(CompletePresenterData(
+                startDate: testData.startDate, endDate: testData.endDate)));
+      });
     });
   });
 }
