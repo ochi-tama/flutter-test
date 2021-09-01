ff18124a9b24902f256c3256b569fe8ba848a1e0
ochi-tama <per.raid@gmail.com>
2021-09-01 23:42:36 +0900

    test: add a test case

--- test/application/wearing_timer/complete/complete_interactor_test.dart
+++ test/application/wearing_timer/complete/complete_interactor_test.dart
@@ -1,11 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:test_app/application/wearing_timer/complete/complete_interactor.dart';
 import 'package:test_app/application/wearing_timer/complete/data/complete_presenter_data.dart';
-import 'package:test_app/application/wearing_timer/find/find_interactor.dart';
-import 'package:test_app/domain/models/wearing_timer/wearing_timer.dart';
 import 'package:test_app/provider.dart';
 
 import '../../../utils/data/fake_wearing_timer.dart';
@@ -13,7 +9,7 @@ import '../../../utils/fake_wearing_timer_repository_impl.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  group("Find interactor test", () {
+  group("Complete interactor test", () {
     late ProviderContainer container;
 
     setUp(() {
@@ -28,7 +24,7 @@ void main() {
     test(
         'Given a wearingTimer is registerd '
         'When complete handler is triggerd '
-        'wearing timer is completed state', () async {
+        'Then presenter is updated by completedTimer', () async {
       final testData = TestWearingTimerData.wearingTimerStarted();
       final repository = FakeWearingTimerRepositoryImpl(wearingTimer: testData);
       final outputPort = container.read(completePresenterProvider.notifier);
@@ -43,6 +39,19 @@ void main() {
           container.read(completePresenterProvider),
           equals(CompletePresenterData(
               startDate: testData.startDate, endDate: testData.endDate)));
+    });
+    test(
+        'Given wearingTimer is not registerd '
+        'When complete handler is triggerd '
+        'Then presenter is not updated', () async {
+      final repository = FakeWearingTimerRepositoryImpl();
+      final outputPort = container.read(completePresenterProvider.notifier);
+      final completeInteractor =
+          CompleteInteractor(repository: repository, outputPort: outputPort);
+      await completeInteractor.handle();
+
+      expect(container.read(completePresenterProvider),
+          equals(CompletePresenterData()));
     });
   });
 }
